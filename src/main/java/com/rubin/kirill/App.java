113437abe38523444e8faefa4676041bb451d65d
Kira Rub <workiteen@gmail.com>
2023-04-08 23:50:53 +0500

    Initial commit

---
+++ src/main/java/com/rubin/kirill/App.java
@@ -0,0 +1,13 @@
+package com.rubin.kirill;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
