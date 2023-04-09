a0f1360622f6bb624f0d89e639324fdc253a49bc
Kira Rub <workiteen@gmail.com>
2023-04-09 12:55:08 +0500

    class Dog

---
+++ src/main/java/Spring_introduction/Dog.java
@@ -0,0 +1,7 @@
+package Spring_introduction;
+
+public class Dog {
+    public void say(){
+        System.out.println("Bow-wow");
+    }
+}
---
+++ src/main/java/Spring_introduction/Test1.java
@@ -0,0 +1,8 @@
+package Spring_introduction;
+
+public class Test1 {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.say();
+    }
+}
