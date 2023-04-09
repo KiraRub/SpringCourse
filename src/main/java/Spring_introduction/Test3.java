794f086fc6754bde58e915eb1fc75c29c5c5e519
Kira Rub <workiteen@gmail.com>
2023-04-09 17:41:29 +0500

    Create bean Person для Spring

--- src/main/java/Spring_introduction/Test3.java
+++ src/main/java/Spring_introduction/Test3.java
@@ -6,9 +6,9 @@ public class Test3 {
     public static void main(String[] args) {
         //Pet pet = new Cat();
         ClassPathXmlApplicationContext Context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        Pet pet = Context.getBean("myPet", Pet.class);
-        Person person = new Person(pet);
-        person.CallYourPet();
-        Context.close();
+        //Pet pet = Context.getBean("myPet", Pet.class);//Получаем из контейнера объект питомца
+        Person person = Context.getBean("myPerson", Person.class);//Получаем из контейнера объект человека
+        person.CallYourPet();//Вызов метода питомца из класса человека
+        Context.close();//Закрытие бина
     }
 }
