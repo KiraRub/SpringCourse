70254247ffac036f2a8dda2c40fb3b8a42f6ea33
Kira Rub <workiteen@gmail.com>
2023-04-12 21:36:27 +0500

    setPet создание ссеттера животного

--- src/main/java/Spring_introduction/Person.java
+++ src/main/java/Spring_introduction/Person.java
@@ -3,12 +3,21 @@ package Spring_introduction;
 public class Person {
     private Pet pet;
 
-    public Person(Pet pet) {
+//    public Person(Pet pet) {
+//        System.out.println("Person bean is created!");
+//        this.pet = pet;
+//    }
+    public Person(){
         System.out.println("Person bean is created!");
+    }
+    public void setPet(Pet pet){
+        System.out.println("Class Person: set pet");
         this.pet = pet;
     }
     public void CallYourPet(){
         System.out.println("HelloMyLovelyPet");
         pet.say();
     }
+
+
 }
